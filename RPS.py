b34519d81d10d9ec353351a648e67242b4a845df
Suchismita Bose <133328192+psychomita@users.noreply.github.com>
2025-07-29 00:11:23 +0530

    refactor: improve player function logic and update main.py for clarity

--- RPS.py
+++ RPS.py
@@ -1,10 +1,25 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+import random
 
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    if prev_play:
+        opponent_history.append(prev_play)
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    if len(opponent_history) < 3:
+        return random.choice(["R", "P", "S"])
 
-    return guess
+    guess_table = {}
+    for i in range(len(opponent_history) - 2):
+        key = "".join(opponent_history[i:i+2])
+        next_move = opponent_history[i + 2]
+        if key in guess_table:
+            guess_table[key][next_move] = guess_table[key].get(next_move, 0) + 1
+        else:
+            guess_table[key] = {next_move: 1}
+
+    last_two = "".join(opponent_history[-2:])
+    prediction = "R"
+    if last_two in guess_table:
+        prediction = max(guess_table[last_two], key=guess_table[last_two].get)
+
+    counter_moves = {'R': 'P', 'P': 'S', 'S': 'R'}
+    return counter_moves[prediction]
